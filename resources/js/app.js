e7ca7b6c41bb8371e50e1d442f46256afe865aa7
SundayCoder81 <richard.bogu@gmail.com>
2024-05-14 21:36:33 +0300

    CC-5: Added vuetify for some vue components

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,17 @@ import { createApp } from 'vue';
 import CommissionCalculator from './components/pages/calculator/Index.vue';
 import { i18nVue } from 'laravel-vue-i18n'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
 createApp({})
     .use(i18nVue, {
         lang: 'pt',
@@ -11,5 +22,6 @@ createApp({})
             return langs[`../../lang/${lang}.json`].default;
         },
     })
+    .use(vuetify)
     .component('commission-calculator', CommissionCalculator)
     .mount('#app')
